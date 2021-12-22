9008d396fc814948a43c3fc02670dea887d34c01
HoracioAldoTore <hoalto@gmail.com>
2021-12-22 13:27:28 -0300

    Primera versión funcional exitosa.
    Primera versión funcional exitosa.

---
+++ MultiSelect/ComponentMultiSelectTore/TItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiSelect.ComponentMultiSelectTore
+{
+    /// <summary>
+    /// Representa un ítem de la lista seleccionable.
+    /// </summary>
+    public class TItem
+    {
+        /// <summary>
+        /// Indica si el ítem está o no seleccionado (chequeado).
+        /// </summary>
+        public bool Checked { set; get; }
+
+        /// <summary>
+        /// Establece el valor que identifica inequívocamente a un ítem.
+        /// </summary>
+        public string Value { set; get; }
+
+        /// <summary>
+        /// Texto que se muestra al usuario, junto al checkbox.
+        /// </summary>
+        public string Label { set; get; }
+    }
+}
---
+++ MultiSelect/ComponentMultiSelectTore/TOrderByTypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiSelect.ComponentMultiSelectTore
+{
+    public enum TOrderByTypes
+    {
+        /// <summary>
+        /// Ordena por la etiqueta o texto que ve el usuario.
+        /// </summary>
+        Label,
+        /// <summary>
+        /// Ordena por el valor, no es visto por el usuario.
+        /// </summary>
+        Value,
+        /// <summary>
+        /// Muestra los items marcados primero y los no marcados después.
+        /// </summary>
+        Checked,
+        /// <summary>
+        /// No ordena, respetando de esta manera el orden natural o predefinido.
+        /// </summary>
+        Nothing
+    }
+}
